ec9e3402aec485e3913dafd135dc5751f7c74267
microsoftdealer <mishagarin@gmail.com>
2021-10-22 15:31:26 +0300

    Fix in handlers import

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -4,8 +4,6 @@ from .b3golden import dp
 from .full_zh import dp
 from .history import dp
 from .donate import dp
-from .nat import dp
-from .unnat import dp
 from .echo import dp
 
 __all__ = ["dp"]
